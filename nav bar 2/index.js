99824ecd740c2891eaa47e584fb92f04a2ae44bd
MarvelDoh <97812114+MarvelDoh@users.noreply.github.com>
2024-04-25 17:21:39 +0100

    Add files via upload

---
+++ nav bar 2/index.js
@@ -0,0 +1,10 @@
+let menuList = document.getElementById("menuList") 
+let bilist = document.getElementById("bilist")
+bilist.addEventListener("click", marvel);
+function marvel(){
+ if(menuList.style.display == "none")
+ {  menuList.style.display = "block"
+ }else{
+     menuList.style.display = "none";
+ }
+}
